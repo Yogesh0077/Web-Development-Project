2d6ffd696d93cef994808d9b05955490cbe150d5
Yogesh0077 <cresthayoogeeshh@gmail.com>
2025-02-14 08:12:08 +0545

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,5 @@
+//JavaScript for "Browse Our Tours" Button functionality
+document.getElementById('browseToursBtn').addEventListener('click', function() {
+    //Show the tours section when button is clicked
+    document.getElementById('tourList').classList.remove('hidden');
+});
